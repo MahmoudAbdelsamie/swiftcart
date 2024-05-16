8f06bf099a96df351dc411d92b03f07822ba693f
MahmoudAbdelsamie <msomaa38@gmail.com>
2024-05-16 18:43:53 +0300

    Create Get User By Id For admin To get specific user & its address

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,8 @@
-// GET /api/admin/users
-
 const pool = require("../config/database");
 
-// return users & their addresses
+
+
+// get users & their addresses
 exports.getUsers = async (req, res, next) => {
     const query = `
         SELECT
@@ -21,7 +21,9 @@ exports.getUsers = async (req, res, next) => {
     try {
         const users = await pool.query(query);
         if(users.rowCount < 1) {
-            return res.json({message: 'No Users Founded!'});
+            return res.json({
+                message: 'No Users Found!'
+            });
         }
         return res.status(200).send({
             status: 'success',
@@ -36,3 +38,46 @@ exports.getUsers = async (req, res, next) => {
         })
     }
 }
+
+
+
+// get specific user & address by id 
+exports.getUserById = async (req, res, next) => {
+    const userId = req.params.id;
+    const query = `
+    SELECT
+        u.username AS username,
+        u.email AS email,
+        u.role as role,
+        u.created_at AS sign_in_date,
+        ad.street AS street,
+        ad.city AS city ,
+        ad.state AS state,
+        ad.zip AS zip_code,
+        ad.country AS country
+    FROM users u
+    JOIN addresses ad ON u.id = ad.user_id
+    WHERE u.id = $1;
+    `;
+    try {
+        const user = await pool.query(query, [userId]);
+        if(user.rowCount < 1) {
+            return res.json({
+                message: 'No User Found'
+            })
+        }
+        return res.status(200).send({
+            status: 'success',
+            message: 'User Retrieved',
+            data: user.rows[0]
+        })
+    } catch(err) {
+        return res.status(500).send({
+            status: 'error',
+            message: 'Internal Server Error',
+            error: err.message 
+        })
+    } 
+
+}
+
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,10 @@
-const { getUsers } = require("../controllers/admin");
+const { getUsers, getUserById } = require("../controllers/admin");
 const { isAdmin } = require("../middlewares/admin");
 const { isAuthorized } = require("../middlewares/user");
 
 const router = require("express").Router();
 
 router.get('/admin/users', isAuthorized, isAdmin, getUsers);
-
+router.get('/admin/users/:id', isAuthorized, isAdmin, getUserById);
 
 module.exports = router;
