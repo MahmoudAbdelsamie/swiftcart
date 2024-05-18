5aad1eb20fedac0724432d1ea6459644cce1c0b5
MahmoudAbdelsamie <msomaa38@gmail.com>
2024-05-18 17:42:08 +0300

    Create Filtering, Pagination, Search, and Ranking For Get-Products

--- controllers/product.js
+++ controllers/product.js
@@ -1,29 +1,72 @@
 const pool = require("../config/database");
 
-exports.getProducts = async (req, res, next) => {
-    const query = 'SELECT * FROM products;';
 
+exports.getProducts = async (req, res, next) => {
+    const {
+        query = '',
+        category,
+        priceMin,
+        priceMax,
+        page = 1,
+        limit = 10
+    } = req.query;
+
+    let sqlQuery = `
+        SELECT p.*, c.name AS category_name, ts_rank(p.search_vector, plainto_tsquery($1)) AS rank
+        FROM products p
+        JOIN categories c ON p.category_id = c.id
+        WHERE plainto_tsquery($1) @@ p.search_vector
+    `;
+    let queryParams = [query];
+
+    if (category) {
+        sqlQuery += ` AND c.name = $${queryParams.length + 1}`;
+        queryParams.push(category);
+    }
+    if (priceMin) {
+        sqlQuery += ` AND p.price >= $${queryParams.length + 1}`;
+        queryParams.push(priceMin);
+    }
+    if (priceMax) {
+        sqlQuery += ` AND p.price <= $${queryParams.length + 1}`;
+        queryParams.push(priceMax);
+    }
+
+    sqlQuery += `
+        ORDER BY rank DESC
+        LIMIT $${queryParams.length + 1} OFFSET $${queryParams.length + 2}
+    `;
+    queryParams.push(limit, (page - 1) * limit);
+
+    console.log(sqlQuery)
+    console.log(queryParams);
     try {
-        const products = await pool.query(query);
-        if(products.rowCount < 1) {
+        const productsResult = await pool.query(sqlQuery, queryParams);
+        if (productsResult.rowCount < 1) {
             return res.status(404).send({
                 status: 'fail',
                 message: 'No Products Found!'
-            })
+            });
         }
+        const products = productsResult.rows;
         return res.status(200).send({
             status: 'success',
             message: 'Products Retrieved',
-            data: products.rows
-        })
-    } catch(err) {
+            data: products
+        });
+    } catch (err) {
         return res.status(500).send({
             status: 'error',
             message: 'Internal Server Error',
             error: err.message
-        })
+        });
     }
-}
+};
+
+
+
+
+
 
 exports.addProduct = async (req, res, next) => {
     const query = 'INSERT INTO products (name, description, price, stock, category_id) VALUES ($1, $2, $3, $4, $5)';
