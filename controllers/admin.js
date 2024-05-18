fabccb01e4d0eb4d8e9ba6eb39c6d9e7601aaedb
MahmoudAbdelsamie <msomaa38@gmail.com>
2024-05-18 20:47:09 +0300

    Create Add-Product By Admin with image upload and encoding to base64

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,8 @@
 const pool = require("../config/database");
 
-
-
 // get users & their addresses
 exports.getUsers = async (req, res, next) => {
-    const query = `
+  const query = `
         SELECT
 	        u.username AS username,
             u.email AS email,
@@ -17,34 +15,32 @@ exports.getUsers = async (req, res, next) => {
             ad.country AS country
         FROM users u
         JOIN addresses ad ON u.id = ad.user_id; 
-    `; 
-    try {
-        const users = await pool.query(query);
-        if(users.rowCount < 1) {
-            return res.json({
-                message: 'No Users Found!'
-            });
-        }
-        return res.status(200).send({
-            status: 'success',
-            message: 'Users Retrieved',
-            data: users.rows
-        })
-    } catch(err) {
-        return res.status(500).send({
-            status: 'error',
-            message: 'Internal Server Error',
-            error: err.message 
-        })
+    `;
+  try {
+    const users = await pool.query(query);
+    if (users.rowCount < 1) {
+      return res.json({
+        message: "No Users Found!",
+      });
     }
-}
+    return res.status(200).send({
+      status: "success",
+      message: "Users Retrieved",
+      data: users.rows,
+    });
+  } catch (err) {
+    return res.status(500).send({
+      status: "error",
+      message: "Internal Server Error",
+      error: err.message,
+    });
+  }
+};
 
-
-
-// get specific user & address by id 
+// get specific user & address by id
 exports.getUserById = async (req, res, next) => {
-    const userId = req.params.id;
-    const query = `
+  const userId = req.params.id;
+  const query = `
     SELECT
         u.username AS username,
         u.email AS email,
@@ -59,49 +55,79 @@ exports.getUserById = async (req, res, next) => {
     JOIN addresses ad ON u.id = ad.user_id
     WHERE u.id = $1;
     `;
-    try {
-        const user = await pool.query(query, [userId]);
-        if(user.rowCount < 1) {
-            return res.json({
-                message: 'No User Found'
-            })
-        }
-        return res.status(200).send({
-            status: 'success',
-            message: 'User Retrieved',
-            data: user.rows[0]
-        })
-    } catch(err) {
-        return res.status(500).send({
-            status: 'error',
-            message: 'Internal Server Error',
-            error: err.message 
-        })
-    } 
-
-}
+  try {
+    const user = await pool.query(query, [userId]);
+    if (user.rowCount < 1) {
+      return res.json({
+        message: "No User Found",
+      });
+    }
+    return res.status(200).send({
+      status: "success",
+      message: "User Retrieved",
+      data: user.rows[0],
+    });
+  } catch (err) {
+    return res.status(500).send({
+      status: "error",
+      message: "Internal Server Error",
+      error: err.message,
+    });
+  }
+};
 
 exports.deleteUserById = async (req, res, next) => {
-    const userId = req.params.id;
-    const query = 'DELETE FROM users WHERE id = $1;';
-    const getUserQuery = 'SELECT * FROM users WHERE id = $1;'
+  const userId = req.params.id;
+  const query = "DELETE FROM users WHERE id = $1;";
+  const getUserQuery = "SELECT * FROM users WHERE id = $1;";
+  try {
+    const { rows } = await pool.query(getUserQuery, [userId]);
+    if (rows.length < 1) {
+      return res.json({
+        message: "No User Found",
+      });
+    }
+    await pool.query(query, [userId]);
+    return res.status(200).send({
+      status: "success",
+      message: "User Deleted",
+    });
+  } catch (err) {
+    return res.status(500).send({
+      status: "error",
+      message: "Internal Server Error",
+      error: err.message,
+    });
+  }
+};
+
+exports.addProduct = async (req, res, next) => {
+
+    const {
+        name,
+        description,
+        price,
+        stock,
+        category_id
+    } = req.body;
+    const query = `
+        INSERT INTO products (name, description, price, stock, category_id, image)
+        VALUES ($1, $2, $3, $4, $5, $6) RETURNING id, name, description, price, stock, category_id, image;
+    `;
     try {
-        const { rows } = await pool.query(getUserQuery, [userId]);
-        if(rows.length < 1) {
-            return res.json({
-                message: 'No User Found'
-            })
-        }
-        await pool.query(query, [userId]);
-        return res.status(200).send({
+        const imageBase64 = req.file.buffer.toString("base64");
+        const productResult = await pool.query(query, [name, description, price, stock, category_id, imageBase64]);
+        const newProduct = productResult.rows[0];
+        return res.status(201).send({
             status: 'success',
-            message: 'User Deleted'
+            message: 'Product Added',
+            data: newProduct
         })
     } catch(err) {
         return res.status(500).send({
             status: 'error',
-            message: 'Internal Server Error',
-            error: err.message 
+            message : 'Internal Server Error',
+            error: err.message
         })
     }
-}
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,24 @@
-const { getUsers, getUserById, deleteUserById } = require("../controllers/admin");
+const multer = require('multer');
+
+const { getUsers, getUserById, deleteUserById, addProduct } = require("../controllers/admin");
 const { isAdmin } = require("../middlewares/admin");
 const { isAuthorized } = require("../middlewares/user");
 
 const router = require("express").Router();
 
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage
+});
+
+// POST Add-Product
+router.post('/admin/products', isAuthorized, isAdmin, upload.single("image"), addProduct)
+
+// Get All Users
 router.get('/admin/users', isAuthorized, isAdmin, getUsers);
+// Get User By Id 
 router.get('/admin/users/:id', isAuthorized, isAdmin, getUserById);
+// Delete User By id
 router.delete('/admin/users/:id', isAuthorized, isAdmin, deleteUserById);
 
 module.exports = router;
