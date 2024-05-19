5621eab46f298d4e7ca533cb307510c09817025c
MahmoudAbdelsamie <msomaa38@gmail.com>
2024-05-19 06:45:35 +0300

    Create Get-Sales-Reports For Admin Endpoint

--- routes/admin.js
+++ routes/admin.js
@@ -9,6 +9,7 @@ const {
   deleteProductById,
   getProducts,
   getOrders,
+  getSalesReports,
 } = require("../controllers/admin");
 const { getProductById } = require("../controllers/product");
 const { isAdmin } = require("../middlewares/admin");
@@ -60,5 +61,8 @@ router.get("/admin/users/:id", isAuthorized, isAdmin, getUserById);
 
 // Delete User By id
 router.delete("/admin/users/:id", isAuthorized, isAdmin, deleteUserById);
+
+// Get Sales Reports
+router.get('/admin/reports/sales', isAuthorized, isAdmin, getSalesReports)
 
 module.exports = router;
