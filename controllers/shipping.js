e062fa9a112df78e9452178d0eb286fe39c5fb4d
MahmoudAbdelsamie <msomaa38@gmail.com>
2024-05-19 17:18:03 +0300

    Create Get-Shipping-Status Endpoint

--- controllers/shipping.js
+++ controllers/shipping.js
@@ -31,3 +31,47 @@ exports.addShipping = async (req, res, next) => {
         });
     }
 };
+
+
+exports.getShippingStatus = async (req, res, next) => {
+    const { orderId } = req.params;
+    const query = `
+        SELECT 
+            s.status, 
+            s.tracking_number, 
+            s.estimated_delivery, 
+            s.created_at, 
+            o.total, 
+            o.status AS order_status, 
+            a.street, 
+            a.city, 
+            a.state, 
+            a.zip, 
+            a.country 
+        FROM shipments s
+        JOIN orders o ON s.order_id = o.id
+        JOIN addresses a ON o.shipping_address_id = a.id
+        WHERE s.order_id = $1;
+    `;
+    try {
+        const result = await pool.query(query, [orderId]);
+        if(result.rowCount < 1) {
+            return res.status(404).send({
+                status: 'fail',
+                message: 'No Shipping Information Found!'
+            })
+        }
+        const shippingStatus = result.rows[0];
+        return res.status(200).send({
+            status: 'success',
+            message: 'Shipping Status Retrieved',
+            data: shippingStatus
+        })
+    } catch (err) {
+        return res.status(500).send({
+            status: 'error',
+            message: 'Internal Server Error',
+            error: err.message
+        });
+    }
+}
--- routes/shipping.js
+++ routes/shipping.js
@@ -1,9 +1,10 @@
-const { addShipping } = require('../controllers/shipping');
+const { addShipping, getShippingStatus } = require('../controllers/shipping');
 const { isAuthorized } = require('../middlewares/user');
 
 const router = require('express').Router();
 
 router.post('/shipping', isAuthorized, addShipping);
 
+router.get('/shipping/status/:orderId', isAuthorized, getShippingStatus);
 
 module.exports = router;
