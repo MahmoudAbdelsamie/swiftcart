b30a27ba3ea8b8f08a74e79de0ac4cdb55df378a
MahmoudAbdelsamie <msomaa38@gmail.com>
2024-05-19 12:28:09 +0300

    Fix Conflicts

--- controllers/product.js
+++ controllers/product.js
@@ -114,39 +114,6 @@ exports.getProductCategories = async (req, res, next) => {
     }
 }
 
-exports.editProduct = async (req, res, next) => {
-    const query = 'UPDATE products SET name=$1, description=$2, price=$3, stock=$4, category_id=$5 WHERE id=$6;';
-    const productQuery = 'SELECT * FROM products WHERE id=$1;'
-    try {
-        const {
-            name,
-            description, 
-            price,
-            stock,
-            category_id 
-        } = req.body;
-        const { id } = req.params;
-        const product = await pool.query(productQuery, [id]);
-        if(product.rowCount < 1) {
-            return res.status(404).send({
-                status: 'fail',
-                message: 'No Product Found!'
-            })
-        }
-        await pool.query(query, [name, description, price, stock, category_id, id]);
-        return res.status(200).send({
-            status: 'success',
-            message: 'Product Updated'
-        })
-
-    } catch(err) {
-        return res.status(500).send({
-            status: 'error',
-            message: 'Internal Server Error',
-            error: err.message
-        })
-    }
-}
 
 exports.getProductById = async (req, res, next) => {
     const query = 'SELECT * FROM products WHERE id=$1;';
@@ -173,20 +140,3 @@ exports.getProductById = async (req, res, next) => {
     }
 }
 
-exports.deleteProduct = async (req, res, next) => {
-    const query = 'DELETE FROM products WHERE id=$1;';
-    try {
-        const { id } = req.params;
-        await pool.query(query, [id]);
-        return res.status(200).send({
-            status: 'success',
-            message: 'Product Deleted'
-        })
-    } catch(err) {
-        return res.status(500).send({
-            status: 'error',
-            message: 'Internal Server Error',
-            error: err.message
-        })
-    }
-}
